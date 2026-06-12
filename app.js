ea4d4733562b94b3a277367d881ca18602daeb09
Víttor Trigone Dias <Vittor.Trigone.Dias@gmail.com>
2026-06-12 09:50:44 -0300

    style: espacamento da etiqueta de nivel pra borda da estante

--- app.js
+++ app.js
@@ -2481,7 +2481,7 @@ window.renderRackVisualizer = function() {
         labelContainer.style.display = 'flex';
         labelContainer.style.alignItems = 'center';
         labelContainer.style.gap = '0.5rem';
-        labelContainer.style.left = '-95px';
+        labelContainer.style.left = '-120px';
         
         const input = document.createElement('input');
         input.type = 'text';
